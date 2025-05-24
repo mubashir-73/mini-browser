1649db7b0e353fd405a2878807e203b508a761b1
mubashir-73 <mubashirsheriff7@gmail.com>
2025-05-24 19:23:25 +0530

    Add custom port access,https header access

--- browser.py
+++ browser.py
@@ -1,23 +1,41 @@
 import socket
+import ssl
 
 
 class URL:
     def __init__(self, url):
         self.scheme, url = url.split("://", 1)
-        assert self.scheme == "http"
+        assert self.scheme in ["http", "https"]
+        if self.scheme == "http":
+            self.port = 80
+        elif self.scheme == "https":
+            self.port = 443
         if "/" not in url:
             url = url + "/"
         self.host, url = url.split("/", 1)
+        if ":" in self.host:
+            self.host, port = self.host.split(":", 1)
+            self.port = int(port)
         self.path = "/" + url
 
     def request(self):
         s = socket.socket(
             family=socket.AF_INET, type=socket.SOCK_STREAM, proto=socket.IPPROTO_TCP
         )
-        s.connect((self.host, 80))
+        s.connect((self.host, self.port))
+        if self.scheme == "https":
+            ctx = ssl.create_default_context()
+            s = ctx.wrap_socket(s, server_hostname=self.host)
         request = "GET {} HTTP/1.0\r\n".format(self.path)
         request += "Host: {} \r\n".format(self.host)
+        headers = {
+            "User-Agent": "Mubsurf",
+            "Connection": "close",
+        }
+        for key, value in headers.items():
+            request += "{}: {}\r\n".format(key, value)
         request += "\r\n"
+        print(request)
         s.send(request.encode("utf8"))
         response = s.makefile("r", encoding="utf8", newline="\r\n")
         status_line = response.readline()
